f996caf6b521c545f1b4c2bf532cbfe34058dfb8
di-ja <dirk@mousezone.de>
2020-07-18 12:29:15 +0200

    40_ZeichenkettenTrennungsMaschine

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("input")
+const word = document.getElementById("word")
+const davor = document.getElementById("davor")
+const danach = document.getElementById("danach")
+const before = document.getElementById("before")
+const after = document.getElementById("after")
+
+let trennen = () => {
+    let index = input.value.indexOf(word.value)
+    if (index == -1) {
+        before.innerHTML = "das war wohl nix"
+        after.innerHTML = input.value
+        return;
+    }
+    if (davor.checked) {
+        before.innerHTML = input.value.slice(0, index)
+        after.innerHTML = input.value.slice(index)
+    } else {
+        before.innerHTML = input.value.slice(0, index + word.value.length)
+        after.innerHTML = input.value.slice(index + word.value.length)
+    }
+}
+    
